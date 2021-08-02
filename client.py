2b3aa7352b4b778577580339d100382e1f5ec462
Yuri Batista <yuri.1115@hotmail.com>
2021-08-02 17:17:32 -0300

    retirado window.mainloop, e adicionado dicionario

--- client.py
+++ client.py
@@ -1,12 +1,19 @@
-import tkinter
-from PIL import ImageTk, Image
 from functools import partial
 import socket
-import _thread
 import time
+
 client = 0
 chances = 6
 palavra = 'null'
+
+def Main():
+    print('Bem-vindo!! Este é o cliente')
+    print('Vamos começar o jogo da forca')
+
+    try:
+        socketCreation()
+    except Exception as e: print(e)
+
 def sendChosenLetter (ltr):
     client.send(ltr.encode('ascii'))  
     
@@ -69,38 +76,40 @@ def desenhar (chances):
         print('|')
             
 def socketCreation():
-    c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    c.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+    clientTCP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     host = '127.0.0.1'
     port = 8000
-    c.connect((host,port))
+    clientTCP.connect((host,port))
     global client
-    client = c
+    client = clientTCP
     global chances
     global palavra
     while chances > 0:
         msg= 'null'
         if(palavra == 'null'):
-            palavra = c.recv(2048).decode('ascii')
+            palavra = clientTCP.recv(2048).decode('ascii')
+            palavraDescoberta = clientTCP.recv(2048).decode('ascii')
             desenhar(chances)
         print('   ' + palavra)
         ltr = input()
         sendChosenLetter(ltr)
-        msg= c.recv(2048).decode('ascii')
+        msg= clientTCP.recv(2048).decode('ascii')
         if(msg == 'errou'):
             chances -= 1
             print('Errou')
             if(chances == 0):
                 print('Perdeu!')
+                print('A palavra era: ' + palavraDescoberta)
         elif(msg.find('-') != -1):
             palavra = msg
             print('Acertou')
         elif(msg.find('-') == -1 and chances > 0):
             palavra = msg
+            desenhar(chances)
+            print('   ' + palavra)
             print('Ganhou!')
-
+            break
         desenhar(chances)
-        
-_thread.start_new_thread(socketCreation, () )
-window = tkinter.Tk()
-window.mainloop()
+
+if __name__ == '__main__':
+    Main()        
--- server.py
+++ server.py
@@ -1,45 +1,52 @@
 import socket
 import random
-try:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
-    host = '127.0.0.1'
-    port = 8000
-    s.bind((host,port))
-    s.listen(5)
-    c,ad = s.accept()
-    print('Connection Established with: '+ad[0])
-    palavras = ['jogador']
-    start = True
-    palavraAleatoria = ''
-    palavraAleatoriaEscondida = ''
-    while True:
-        if(start):
-            palavraAleatoria = palavras [random.randint(0,len(palavras)-1)]
-            palavraAleatoriaEscondida = "-"*len(palavraAleatoria)
-            c.send(palavraAleatoriaEscondida.encode('ascii'))
-            start = False
-        else:
-            letraRecebida = c.recv(2048).decode('ascii')
-            if(letraRecebida == 'reset'):
-                start = True
-                palavraAleatoria = ''
-                palavraAleatoriaEscondida = ''
-            else:    
-                index = []
-                aux = ''
-                if (palavraAleatoria.find(letraRecebida) != -1):
-                    for i in range(0,len(palavraAleatoria)):
-                        if(letraRecebida == palavraAleatoria[i]):
-                            aux += letraRecebida
-                        else :
-                            aux += palavraAleatoriaEscondida[i]
-                    palavraAleatoriaEscondida = aux
-                    c.send(palavraAleatoriaEscondida.encode('ascii'))
-                else :
-                    c.send('errou'.encode('ascii'))     
-    s.close()
-except socket.error as e :
-    print(e)
-except KeyboardInterrupt :
-    print('chat ended')
+
+def main():
+    print('Bem-vindo!! Este é o servidor')
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        host = '127.0.0.1'
+        port = 8000
+        s.bind((host,port))
+        s.listen(5)
+        c,ad = s.accept()
+        print('Connection Established with: '+ad[0])
+        meuArquivo = open('dicionario.txt')
+        palavras = meuArquivo.read().split('\n')
+        start = True
+        palavraAleatoria = ''
+        palavraAleatoriaEscondida = ''
+        while True:
+            if(start):
+                palavraAleatoria = palavras [random.randint(0,len(palavras)-1)]
+                palavraAleatoriaEscondida = "-"*len(palavraAleatoria)
+                c.send(palavraAleatoriaEscondida.encode('ascii'))
+                c.send(palavraAleatoria.encode('ascii'))
+                start = False
+            else:
+                letraRecebida = c.recv(2048).decode('ascii')
+                if(letraRecebida == 'reset'):
+                    start = True
+                    palavraAleatoria = ''
+                    palavraAleatoriaEscondida = ''
+                else:    
+                    index = []
+                    aux = ''
+                    if (palavraAleatoria.find(letraRecebida) != -1):
+                        for i in range(0,len(palavraAleatoria)):
+                            if(letraRecebida == palavraAleatoria[i]):
+                                aux += letraRecebida
+                            else :
+                                aux += palavraAleatoriaEscondida[i]
+                        palavraAleatoriaEscondida = aux
+                        c.send(palavraAleatoriaEscondida.encode('ascii'))
+                    else :
+                        c.send('errou'.encode('ascii'))     
+        s.close()
+    except socket.error as e :
+        print(e)
+    except KeyboardInterrupt :
+        print('chat ended')
+
+if __name__ == "__main__":
+    main()
